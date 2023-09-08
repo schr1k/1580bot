a406e9910170f4a424a1e6fe03c0e91e7ef28795
shr1k <s.ivan2006@yandex.ru>
2023-09-08 22:15:00 +0300

    feat: schedule read algorithm

--- load.py
+++ load.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import json
+import simplejson as json
 
 df = pd.read_excel('https://lycu1580.mskobr.ru/files/attach_files/rasp1k.xlsx', sheet_name='Расписание').T.values.tolist()
 
-for i in df:
-    for j in range(2, 10):
-        print(i[j])
-with open('schedule.json', 'w', encoding='utf-8') as f:
-    json.dump(df, f, indent=4, ensure_ascii=False)
+with open('excel.json', 'w', encoding='utf-8') as f:
+    json.dump(df, f, indent=4, ensure_ascii=False, ignore_nan=True)
--- test.py
+++ test.py
@@ -1,17 +1,33 @@
 import json
 
-import pandas as pd
-
-df = pd.read_json('schedule.json').T
+with open('excel.json', encoding='utf-8') as f:
+    excel = json.load(f)
 
 weekdays = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
 
-js = dict()
-for col in range(2, len(df)):
-    psl = {}
-    for i in weekdays:
-        psl[i] = {i: j for i, j in zip(range(1, 9), df[col][:8])}
-    js[df[col][8]] = psl
+for i in range(len(excel)):
+    sp = [excel[i][8]] + excel[i]
+    excel[i] = sp
 
+schedule = {}
 
-print(json.dumps(js, indent=4, ensure_ascii=False))
+for column in excel[2:-2:2]:
+    for c, i in enumerate(range(0, len(column), 9)):
+        class_name = column[0]
+        day_schedule = {i: j for i, j in zip(range(1, 9), column[i + 1: i + 9])}
+        if column[i] in schedule:
+            if weekdays[c] in schedule[column[i]]:
+                schedule[column[i]][weekdays[c]] = day_schedule
+            else:
+                schedule[column[i]][weekdays[c]] = {}
+                schedule[column[i]][weekdays[c]] = day_schedule
+        else:
+            schedule[column[i]] = {}
+            if weekdays[c] in schedule[column[i]]:
+                schedule[column[i]][weekdays[c]] = day_schedule
+            else:
+                schedule[column[i]][weekdays[c]] = {}
+                schedule[column[i]][weekdays[c]] = day_schedule
+
+with open('schedule.json', 'w', encoding='utf-8') as f:
+    json.dump(schedule, f, indent=4, ensure_ascii=False)
