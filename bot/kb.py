72b8e2867a8e4a41251e6414492b3aa8c9956c8d
melkhom <jigoku.tyan@gmail.com>
2023-09-09 19:36:59 +0300

    feat: added text format funcs

--- bot/kb.py
+++ bot/kb.py
@@ -3,4 +3,5 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 get_schedule = InlineKeyboardButton(text='Получить расписание', callback_data='get_schedule')
-main_kb = InlineKeyboardBuilder().add(get_schedule)
+find_teacher = InlineKeyboardButton(text='Найти учителя', callback_data='find_teacher')
+main_kb = InlineKeyboardBuilder().add(get_schedule, find_teacher)
