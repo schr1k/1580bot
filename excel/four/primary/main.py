233632f8b985ae338f90d6ce08a1a080374a9b2b
shr1k <s.ivan2006@yandex.ru>
2023-10-25 21:06:06 +0300

    fix(bot): 4 building generation

--- excel/four/primary/main.py
+++ excel/four/primary/main.py
@@ -5,7 +5,7 @@ from bot import config
 
 
 def make_schedule_4p():
-    df = pd.read_excel('https://lycu1580.mskobr.ru/files/schedule/rasp_symbol_ns_3.xlsx', header=None).T.values.tolist()
+    df = pd.read_excel('https://lycu1580.mskobr.ru/files/schedule/rasp_symbol_ns_4.xlsx', header=None).T.values.tolist()
     with open(f'{config.PROJECT_PATH}/excel/four/primary/excel.json', 'w', encoding='utf-8') as f:
         json.dump(df, f, indent=4, ensure_ascii=False, ignore_nan=True)
 
@@ -33,19 +33,19 @@ def make_schedule_4p():
             for lesson_number, j in zip(range(1, 9), list(lessons)):
                 day_schedule[lesson_number] = j
             day_schedule = {i: j for i, j in zip(range(1, 9), lessons)}
-            if excel[column][i] in schedule:
-                if weekdays[c] in schedule[excel[column][i]]:
-                    schedule[excel[column][i]][weekdays[c]] = day_schedule
+            if excel[column][0] in schedule:
+                if weekdays[c] in schedule[excel[column][0]]:
+                    schedule[excel[column][0]][weekdays[c]] = day_schedule
                 else:
-                    schedule[excel[column][i]][weekdays[c]] = {}
-                    schedule[excel[column][i]][weekdays[c]] = day_schedule
+                    schedule[excel[column][0]][weekdays[c]] = {}
+                    schedule[excel[column][0]][weekdays[c]] = day_schedule
             else:
-                schedule[excel[column][i]] = {}
-                if weekdays[c] in schedule[excel[column][i]]:
-                    schedule[excel[column][i]][weekdays[c]] = day_schedule
+                schedule[excel[column][0]] = {}
+                if weekdays[c] in schedule[excel[column][0]]:
+                    schedule[excel[column][0]][weekdays[c]] = day_schedule
                 else:
-                    schedule[excel[column][i]][weekdays[c]] = {}
-                    schedule[excel[column][i]][weekdays[c]] = day_schedule
+                    schedule[excel[column][0]][weekdays[c]] = {}
+                    schedule[excel[column][0]][weekdays[c]] = day_schedule
 
     with open(config.SCHEDULE_PATH, 'r', encoding='utf-8') as f:
         all_schedule = json.load(f)
